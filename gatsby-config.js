2fd92770858dac27e3efd12ae267e5ffbe416ce0
Scott Arden-Jones <s.ardenjones@gmail.com>
2021-01-31 18:48:19 +0000

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'scanarjod.dev',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This is my new JAM Stack blog, built using Netlify CMS + Gatsby',
   },
   plugins: [
     'gatsby-plugin-postcss',
